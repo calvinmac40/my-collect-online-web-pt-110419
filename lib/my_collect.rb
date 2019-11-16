4df4a8cad5d93085dd2076c78aa3ebdab3294d4c
Calvin McDowell <calvinmac40@hotmail.com>
2019-11-16 01:48:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,6 +2,9 @@ def my_collect(empty_array)
   i = 0
   collect = []
   while i < empty_array.length 
-    collect << (empty_array[i])
+    collect << yield(empty_array[i])
+    i += 1
+  end
+  collect
 end
 
